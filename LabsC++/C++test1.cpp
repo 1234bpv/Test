2817bd11a34fb875eddfef83a94622fcdfa33747
1234bpv <33402738+1234bpv@users.noreply.github.com>
2017-11-21 15:54:37 +0200

    Test Commit
    bgvfcgvfvc Паша олох

---
+++ LabsC++/C++test1.cpp
@@ -0,0 +1,12 @@
+// C++test1.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) 
+{
+	cout << "It's me, your first program.";
+	system("pause");
+	return 0;
+
+}
